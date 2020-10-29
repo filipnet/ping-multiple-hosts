bf64696e101b71a923340e479b5e6f577f05f6fa
Benedikt Filip <benedikt.filip@filipnet.de>
2020-10-29 13:06:20 +0100

    Add response time to output

--- ping-multiple-hosts.ps1
+++ ping-multiple-hosts.ps1
@@ -4,15 +4,18 @@ while ($true) {
     foreach ($name in $names){
       $Time = (Get-Date).ToString(“HH:mm:ss”)
       $Date = Get-Date -Format dd.MM.yyyy
-      if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
-       $Output+= "$Date,$Time,$name,up"
-       Write-Host "$Date,$Time,$Name,up" -ForegroundColor Green
-      }
-      else{
-        $Output+= "$Date,$Time,$name,down"
-        Write-Host "$Date,$Time,$Name,down" -ForegroundColor Red
+      # Test-Connection -TimeoutSeconds parameter available at PSVersion 6 (Check: $PSVersionTable.PSVersion)
+      $response = (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue | measure-Object -Property ResponseTime -Average).average
+      if ($response -ne $null){
+        #$response = ($response -as [int] ) 
+        $Output+= "$Date,$Time,$name,up,$response ms"
+        Write-Host "$Date,$Time,$Name,up,$response ms" -ForegroundColor Green
+      }else{
+        $Output+= "$Date,$Time,$name,down,no response"
+        Write-Host "$Date,$Time,$Name,down,no response" -ForegroundColor Red
       }
     }
     $Output | Out-file results.csv
-    Start-Sleep -Seconds 1
+    # Start-Sleep -Seconds 1
+    # Start-Sleep -Millis 300
 }
